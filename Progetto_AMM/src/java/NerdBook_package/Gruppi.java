65682a4b74d3644506c6405515bb4fc7b61ae343
pisuw <pisuw@LAPTOP-T1FSU316>
2017-05-03 11:14:30 +0200

    backup4 (m3)

---
+++ Progetto_AMM/src/java/NerdBook_package/Gruppi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NerdBook_package;
+
+/**
+ *
+ * @author pisuw
+ */
+
+import NerdBook_package.Post;
+import NerdBook_package.Utenti_registrati;
+
+
+public class Gruppi {
+    
+    private int idgr;
+    private String nomegruppo;
+    private String email;
+    private String data;
+    private String password;
+    private String urlFotoProfilo;
+    
+    
+}
---
+++ Progetto_AMM/src/java/NerdBook_package/Post.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NerdBook_package;
+
+/**
+ *
+ * @author pisuw
+ */
+
+
+
+public class Post {
+    
+    
+    public enum Type {
+        TEXT, IMAGE
+    };
+
+    protected int id;
+    protected Utenti_registrati user;
+    private String content;
+    private Type postType;
+
+    public Post() {
+        id = 0;
+        user = null;
+        content = "";
+        postType = Type.TEXT;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the user
+     */
+    public Utenti_registrati getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(Utenti_registrati user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the postType
+     */
+    public Type getPostType() {
+        return postType;
+    }
+
+    /**
+     * @param postType the postType to set
+     */
+    public void setPostType(Type postType) {
+        this.postType = postType;
+    }
+    
+    
+}
